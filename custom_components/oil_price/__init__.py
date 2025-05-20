2b8e66b1005bbed1de29d4c56d2906e2dbfae401
zhoujunn <61492854+zhoujunn@users.noreply.github.com>
2025-05-20 15:56:31 +0800

    Update __init__.py

--- custom_components/oil_price/__init__.py
+++ custom_components/oil_price/__init__.py
@@ -1,1 +1,13 @@
-"""China Oil Price integration."""
+from .const import DOMAIN
+
+async def async_setup(hass, config):
+    return True
+
+async def async_setup_entry(hass, entry):
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    )
+    return True
+
+async def async_unload_entry(hass, entry):
+    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")

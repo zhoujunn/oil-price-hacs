644de5aff9601838c51ae183c574fed842082016
zhoujunn <61492854+zhoujunn@users.noreply.github.com>
2025-07-03 16:52:31 +0800

    Update __init__.py
    来自：brick713

--- custom_components/oil_price/__init__.py
+++ custom_components/oil_price/__init__.py
@@ -5,9 +5,9 @@ async def async_setup(hass, config):
 
 async def async_setup_entry(hass, entry):
     hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
+        hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     )
     return True
 
 async def async_unload_entry(hass, entry):
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
